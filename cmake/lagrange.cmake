6593f67b29a4df6211777e3180da51a0a205abed
Jérémie Dumas <jdumas@users.noreply.github.com>
2022-06-08 09:52:54 -0700

    Update lagrange.cmake

--- cmake/lagrange.cmake
+++ cmake/lagrange.cmake
@@ -14,6 +14,6 @@ include(FetchContent)
 FetchContent_Declare(
     lagrange
     GIT_REPOSITORY https://github.com/adobe/lagrange.git
-    GIT_TAG v6.0.0
+    GIT_TAG v6.2.0
 )
 FetchContent_MakeAvailable(lagrange)
